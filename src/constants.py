dc2518cde7aabf5254caf97fcb278e31fdc53c65
Sabina Alina PRODAN <saprodan@bitdefender.com>
2024-10-13 21:41:39 +0300

    add score calculation for given dice roll

---
+++ src/constants.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+class ScoreCategory(Enum):
+    ONES = 0
+    TWOS = 1
+    THREES = 2
+    FOURS = 3
+    FIVES = 4
+    SIXES = 5
+    THREE_OF_A_KIND = 6
+    FOUR_OF_A_KIND = 7
+    FULL_HOUSE = 8
+    SMALL_STRAIGHT = 9
+    LARGE_STRAIGHT = 10
+    CHANCE = 11
+    YAHTZEE = 12
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,9 @@
 import random
 
+from collections import Counter
+
+from constants import ScoreCategory
+
 def roll_random_dice(dice_no: int):
     """
     Return an array with `dice_no` random values from 1 to 6.
@@ -20,7 +24,53 @@ def reroll(dice_roll: list[int], to_roll: list[int]):
 def score_roll(dice_roll: list[int]):
     """
     Return list of possible scores for each category of the game
-    with the given dice roll.
+    (excluding bonus) with the given dice roll.
     """
-    raise NotImplementedError()
+    scores = [-1] * 13
 
+    counts = Counter(dice_roll)
+    dice_sum = sum(dice_roll)
+
+    for i in range(6):
+        # -in-a-row scores
+        scores[i] = i * dice_roll.count(i)
+
+    if any(count >= 3 for count in counts.values()):
+        scores[ScoreCategory.THREE_OF_A_KIND.value] = sum(dice_roll)
+    else:
+        scores[ScoreCategory.THREE_OF_A_KIND.value] = 0
+
+    if any(count >= 4 for count in counts.values()):
+        scores[ScoreCategory.FOUR_OF_A_KIND.value] = dice_sum
+    else:
+        scores[ScoreCategory.FOUR_OF_A_KIND.value] = 0
+
+    if sorted(counts.values()) == [2, 3]:
+        scores[ScoreCategory.FULL_HOUSE.value] = 25
+    else:
+        scores[ScoreCategory.FULL_HOUSE.value] = 0
+
+    small_straights = [{1, 2, 3, 4}, {2, 3, 4, 5}, {3, 4, 5, 6}]
+    if any(straight.issubset(dice_roll) for straight in small_straights):
+        scores[ScoreCategory.SMALL_STRAIGHT.value] = 30
+    else:
+        scores[ScoreCategory.SMALL_STRAIGHT.value] = 0
+
+    large_straights = [{1, 2, 3, 4, 5}, {2, 3, 4, 5, 6}]
+    if set(dice_roll) in large_straights:
+        scores[ScoreCategory.LARGE_STRAIGHT.value] = 40
+    else:
+        scores[ScoreCategory.LARGE_STRAIGHT.value] = 0
+
+    scores[ScoreCategory.CHANCE.value] = dice_sum
+
+    if any(count == 5 for count in counts.values()):
+        scores[ScoreCategory.YAHTZEE.value] = 50
+    else:
+        scores[ScoreCategory.YAHTZEE.value] = 0
+
+    return scores
+
+
+# if __name__ == "__main__":
+#     print(score_roll([0, 1, 2, 3, 4]))
